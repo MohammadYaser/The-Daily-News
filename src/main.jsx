caf1e500c84fc98d0982ea1c63f4ed2ff056c9d3
unknown <yasersafi19@outlook.com>
2025-11-09 22:37:02 +0430

    Added routes for home and contact pages

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import Router from './routes'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
+
+
+
+createRoot(document.getElementById('root')).render(
+      
+            <Router />
+      
+    
+)
